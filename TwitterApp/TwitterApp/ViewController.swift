d42372ca941c2fec44b2dc4af7affe08a234a892
Paulo Cesar Morandi Massuci <pmassuci@ciandt.com>
2019-06-16 22:30:45 -0300

    Cria o projeto inicial

---
+++ TwitterApp/TwitterApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwitterApp
+//
+//  Created by Paulo Cesar Morandi Massuci on 16/06/19.
+//  Copyright © 2019 Paulo Cesar Morandi Massuci. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
